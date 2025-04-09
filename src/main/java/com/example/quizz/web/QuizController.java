5b535cbc1388e8c4dafd597d60760c1fc006ad10
Annie Tran <tranthilananh105@gmail.com>
2025-04-09 13:53:32 +0300

    update QuizController, change RestController to Controller

--- src/main/java/com/example/quizz/web/QuizController.java
+++ src/main/java/com/example/quizz/web/QuizController.java
@@ -6,14 +6,23 @@ import com.example.quizz.domain.QuizRepository;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
 
-@RestController
+
+@Controller
 @RequestMapping("/quizzes")
 public class QuizController {
 
     @Autowired
     private QuizRepository quizRepository;
+
+    // Serve the Add Quiz form
+    @GetMapping("/addquiz")
+    public String showAddQuizForm() {
+        return "addquiz";
+    }
 
     // Create a new quiz
     @PostMapping
@@ -25,17 +34,6 @@ public class QuizController {
     public List<Quiz> getQuiz() {
         return quizRepository.findAll();
     }
-
-    @DeleteMapping("/{id}")
-    public String deleteQuiz(@PathVariable Long id) {
-        quizRepository.deleteById(id);
-
-        if (quizRepository.existsById(id) == true) {
-            return "Delete failed";
-        } else {
-            return "Delete success";
-        }
-
-    }
+    
 
 }
