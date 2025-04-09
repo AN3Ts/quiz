8b2d2bd8a77e0f6208aca75169e961620513b0a9
Trang Le <trang.le@myy.haaga-helia.fi>
2025-04-09 13:09:03 +0300

    Added getQuiz methods in QuizController

--- src/main/java/com/example/quizz/web/QuizController.java
+++ src/main/java/com/example/quizz/web/QuizController.java
@@ -2,8 +2,12 @@ package com.example.quizz.web;
 
 import com.example.quizz.domain.Quiz;
 import com.example.quizz.domain.QuizRepository;
+
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
 
 
 @RestController
@@ -18,5 +22,11 @@ public class QuizController {
     public Quiz createQuiz(@RequestBody Quiz quiz) {
         return quizRepository.save(quiz);
     }
+
+    @GetMapping
+    public List<Quiz> getQuiz(){
+        return quizRepository.findAll();
+    }
+    
 
 }
