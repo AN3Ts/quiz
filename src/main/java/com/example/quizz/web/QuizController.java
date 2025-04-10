93ce5e7963c2bee602ecf9d728a30f1f08248679
Trang Le <trang.le@myy.haaga-helia.fi>
2025-04-10 12:35:25 +0300

    Modify the delete quiz function

--- src/main/java/com/example/quizz/web/QuizController.java
+++ src/main/java/com/example/quizz/web/QuizController.java
@@ -3,12 +3,12 @@ package com.example.quizz.web;
 import com.example.quizz.domain.Quiz;
 import com.example.quizz.domain.QuizRepository;
 
+import org.springframework.ui.Model;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 @RequestMapping("/quizzes")
@@ -19,37 +19,44 @@ public class QuizController {
 
     // return all quizzes in UI view
     @GetMapping("/showQuizzes")
-    public String showQuizzesString() {
-        return "quizzes";
+    public String showQuizzesString(Model model) {
+        List<Quiz> quizzes = quizRepository.findAll(); 
+        model.addAttribute("quizzes", quizzes);
+        return "quizzes"; //return quizzes.html
     }
 
     // Serve the Add Quiz form
     @GetMapping("/addquiz")
     public String showAddQuizForm() {
-        return "addquiz";
+        return "addquiz"; //return addquizz.html
     }
 
-    // Create a new quiz
+    // Create and save new quiz
     @PostMapping
     public String createQuiz(@ModelAttribute Quiz quiz) {
         quizRepository.save(quiz);
-        return "redirect:/quizzes";
-    }
+        return "redirect:/quizzes"; 
+    } //successful save currentlt return json of newly added quiz, and not the homepage
 
-    // return json here
+    // return json of all quizzes here with the annottation @ResponseBody
     @GetMapping
+    @ResponseBody
     public List<Quiz> getQuiz() {
         return quizRepository.findAll();
     }
 
-    @DeleteMapping("/{id}")
-    public String deleteQuiz(@PathVariable Long id) {
+    @GetMapping("/{id}") //This has to be a GetMapping, not DeleteMapping, method used below is deleteById
+    public String deleteQuiz(@PathVariable Long id, Model model) {
         quizRepository.deleteById(id);
 
         if (quizRepository.existsById(id)) {
-            return "Delete failed";
+            model.addAttribute("message", "Delete failed"); 
+            model.addAttribute("alertType", "danger"); 
         } else {
-            return "Delete success";
+            model.addAttribute("message", "Delete successful");
+            model.addAttribute("alertType", "success"); 
         }
+
+        return "redirect:../quizzes/showQuizzes";
     }
 }
