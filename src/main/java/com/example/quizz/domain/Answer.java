7323fe72e0683c2a0c3ff32be486c4e58c79a4ec
NguyenTuong0420 <manhtuonga2bd@Gmail.com>
2025-04-11 12:31:57 +0300

    Add Answer entity

---
+++ src/main/java/com/example/quizz/domain/Answer.java
@@ -0,0 +1,63 @@
+package com.example.quizz.domain;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.validation.constraints.NotBlank;
+
+@Entity
+public class Answer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @NotBlank(message = "The answer cannot be blank")
+    private String answerText;
+
+    @NotBlank()
+    private Boolean isCorrect;
+
+    @ManyToOne
+    @JoinColumn(name = "question_id", nullable = false) // Foreign key column in the Answer table
+    @JsonBackReference
+    private Question question;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getAnswerText() {
+        return answerText;
+    }
+
+    public void setAnswerText(String answerText) {
+        this.answerText = answerText;
+    }
+
+    public Boolean getIsCorrect() {
+        return isCorrect;
+    }
+
+    public void setIsCorrect(Boolean isCorrect) {
+        this.isCorrect = isCorrect;
+    }
+
+    public Question getQuestion() {
+        return question;
+    }
+
+    public void setQuestion(Question question) {
+        this.question = question;
+    }
+
+}
