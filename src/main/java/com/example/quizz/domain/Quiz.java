3a27ef687605e1c2732b76d344bb42221284b331
Dozy <dotuan14747@gmail.com>
2025-04-15 09:20:30 +0300

    Remove unused imports

--- src/main/java/com/example/quizz/domain/Quiz.java
+++ src/main/java/com/example/quizz/domain/Quiz.java
@@ -3,8 +3,6 @@ package com.example.quizz.domain;
 import java.util.ArrayList;
 import java.util.List;
 
-import com.fasterxml.jackson.annotation.JsonBackReference;
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonManagedReference;
 
 import jakarta.persistence.CascadeType;
