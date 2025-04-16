0bd1484ce8f658b0609e14edda41f38c393727e4
Trang Le <trang.le@myy.haaga-helia.fi>
2025-04-16 11:28:10 +0300

    stable: try fetching api

--- quiz-student-view/src/App.jsx
+++ quiz-student-view/src/App.jsx
@@ -1,10 +1,25 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
+  const [quizzes, setQuizzes] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const data = await fetch('http://localhost:8080/api/quizzes');
+      setQuizzes(await data.json());
+      
+    };
+    fetchData();
+     
+  
+  },[])
+
+  console.log(quizzes);
+
 
   return (
     <>
@@ -25,6 +40,7 @@ function App() {
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
       </div>
+    
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
