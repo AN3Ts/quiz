1f72e4b4c0aea89bbe6b19795faab073ef920e19
Trang Le <trang.le@myy.haaga-helia.fi>
2025-04-23 11:36:00 +0300

    filtering out unpublished quizzes

--- quiz-student-view/src/App.jsx
+++ quiz-student-view/src/App.jsx
@@ -1,5 +1,5 @@
-import { useEffect, useState } from 'react'
-import './App.css'
+import { useEffect, useState } from 'react';
+import './App.css';
 
 function App() {
   const [quizzes, setQuizzes] = useState([]);
@@ -7,29 +7,26 @@ function App() {
   useEffect(() => {
     const fetchData = async () => {
       const data = await fetch('https://quiz-git-quiz.2.rahtiapp.fi/api/quizzes');
-      setQuizzes(await data.json());
-      
+      const allQuizzes = await data.json();
+      setQuizzes(allQuizzes.filter((quiz) => quiz.published === true));
     };
     fetchData();
-  
-  },[])
+  }, []);
 
   console.log(quizzes);
-
 
   return (
     <>
       <h1>Student view</h1>
       <div className="card">
-        {quizzes.map((quiz) => 
-          (
-            <li key={quiz.id}>{quiz.name}: {quiz.description}</li>
-          )
-        )} 
+        {quizzes.map((quiz) => (
+          <li key={quiz.id}>
+            {quiz.name}: {quiz.description}
+          </li>
+        ))}
       </div>
-
     </>
-  )
+  );
 }
 
-export default App
+export default App;
