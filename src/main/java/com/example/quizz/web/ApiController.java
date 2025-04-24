7101335637b78b58ab9839b814a2208f28c09a7a
Annie Tran <tranthilananh105@gmail.com>
2025-04-25 00:31:01 +0300

    add API for get quiz by ID and get the question with quiz ID

--- src/main/java/com/example/quizz/web/ApiController.java
+++ src/main/java/com/example/quizz/web/ApiController.java
@@ -3,29 +3,33 @@ package com.example.quizz.web;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.quizz.domain.Category;
 import com.example.quizz.domain.CategoryRepository;
+import com.example.quizz.domain.Question;
 import com.example.quizz.domain.Quiz;
 import com.example.quizz.domain.QuizRepository;
 
 @RestController
 @RequestMapping("/api")
-@CrossOrigin(origins="*")
+@CrossOrigin(origins = "*")
 public class ApiController {
 
     @Autowired
     private QuizRepository quizRepository;
     @Autowired
     private CategoryRepository categoryRepository;
-   
-     // Return json of all quizzes here with the annottation @ResponseBody
-    @GetMapping("/quizzes") 
+
+    // Return json of all quizzes here with the annottation @ResponseBody
+    @GetMapping("/quizzes")
     @ResponseBody
     public List<Quiz> getQuiz() {
         List<Quiz> allQuizzes = quizRepository.findAll();
@@ -38,6 +42,20 @@ public class ApiController {
     public List<Category> getCategory() {
         return categoryRepository.findAll();
     }
-    
 
+    @GetMapping("/quizzes/{id}")
+    @ResponseBody
+    public ResponseEntity<Quiz> getQuizById(@PathVariable Long id) {
+        return quizRepository.findById(id)
+                .map(quiz -> new ResponseEntity<>(quiz, HttpStatus.OK))
+                .orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
+    }
+
+    @GetMapping("/quizzes/{id}/questions")
+    @ResponseBody
+    public ResponseEntity<List<Question>> getQuizQuestions(@PathVariable Long id) {
+        return quizRepository.findById(id)
+                .map(quiz -> new ResponseEntity<>(quiz.getQuestions(), HttpStatus.OK))
+                .orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
+    }
 }
