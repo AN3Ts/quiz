e85d10caf0f84a54f650c0c3c9981002c4acef49
Annie Tran <tranthilananh105@gmail.com>
2025-04-25 01:10:51 +0300

    updated the App.jsx with AppBar from MUI

--- quiz-student-view/src/App.jsx
+++ quiz-student-view/src/App.jsx
@@ -1,12 +1,15 @@
-import { useEffect, useState } from 'react';
-import './App.css';
+import { useEffect, useState } from "react";
+import { AppBar, Typography, Box, Toolbar } from "@mui/material";
+import "./App.css";
 
 function App() {
   const [quizzes, setQuizzes] = useState([]);
 
   useEffect(() => {
     const fetchData = async () => {
-      const data = await fetch('https://quiz-git-quiz.2.rahtiapp.fi/api/quizzes');
+      const data = await fetch(
+        "https://quiz-git-quiz.2.rahtiapp.fi/api/quizzes"
+      );
       //const data = await fetch('http://localhost:8080/api/quizzes');
       setQuizzes(await data.json());
     };
@@ -16,16 +19,39 @@ function App() {
   console.log(quizzes);
 
   return (
-    <>
-      <h1>Student view</h1>
-      <div className="card">
-        {quizzes.map((quiz) => (
-          <li key={quiz.id}>
-            {quiz.name}: {quiz.description}
-          </li>
-        ))}
-      </div>
-    </>
+    <Box
+      sx={{
+        width: "100vw",
+        height: "100vh",
+        display: "flex",
+        flexDirection: "column",
+      }}
+    >
+      <AppBar position="static">
+        <Toolbar>
+          <Typography
+            variant="h6"
+            component="div"
+            sx={{ flexGrow: 1, fontWeight: "bold" }}
+          >
+            Quizzer
+          </Typography>
+        </Toolbar>
+      </AppBar>
+
+      <Box sx={{ padding: 3, flexGrow: 1 }}>
+        <Typography variant="h5">Available Quizzes</Typography>
+        <ul>
+          {quizzes.map((quiz) => (
+            <li key={quiz.id}>
+              <Typography variant="body1">
+                <strong>{quiz.name}</strong>: {quiz.description}
+              </Typography>
+            </li>
+          ))}
+        </ul>
+      </Box>
+    </Box>
   );
 }
 
