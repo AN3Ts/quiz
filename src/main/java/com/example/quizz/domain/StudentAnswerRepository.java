ecd7e4958bb3785488ce6a127d511b6d079d9612
Dozy <dotuan14747@gmail.com>
2025-04-28 16:32:16 +0300

    Created findByAnswerQuestionId to find all answers given a question id

--- src/main/java/com/example/quizz/domain/StudentAnswerRepository.java
+++ src/main/java/com/example/quizz/domain/StudentAnswerRepository.java
@@ -1,9 +1,11 @@
 package com.example.quizz.domain;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface StudentAnswerRepository extends JpaRepository<StudentAnswer, Long> {
-
+    List<StudentAnswer> findByAnswerQuestionId(Long questionId);
 }
