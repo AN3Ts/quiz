a79c5c707031d2fd835245eda327d090c554240e
Dozy <dotuan14747@gmail.com>
2025-04-28 15:51:23 +0300

    Removed unsused import in domain/Category and domain/Quiz

--- src/main/java/com/example/quizz/domain/Category.java
+++ src/main/java/com/example/quizz/domain/Category.java
@@ -4,13 +4,11 @@ import java.util.ArrayList;
 import java.util.List;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.annotation.JsonManagedReference;
 
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.GenerationType;
 import jakarta.validation.constraints.NotBlank;
