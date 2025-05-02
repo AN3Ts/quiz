ead87d9f87d16169787bb23611c18520a3291034
Dozy <dotuan14747@gmail.com>
2025-05-02 08:25:36 +0300

    Created screens/Quizzes.jsx to show all published quizzes

---
+++ quiz-student-view/src/screens/Quizzes.jsx
@@ -0,0 +1,41 @@
+import useFetchData from "../hooks/useFetchData";
+import { AgGridReact } from "ag-grid-react";
+import { ModuleRegistry } from "ag-grid-community";
+import { ClientSideRowModelModule, ValidationModule } from "ag-grid-community";
+import { Typography } from "@mui/material";
+import "ag-grid-community/styles/ag-theme-alpine.css";
+
+ModuleRegistry.registerModules([ClientSideRowModelModule, ValidationModule]);
+
+const Quizzes = () => {
+  const { data: quizzes } = useFetchData(
+    import.meta.env.VITE_API_URL + "quizzes"
+  );
+
+  console.log(quizzes);
+
+  const colDefs = [
+    { headerName: "Name", field: "name" },
+    { headerName: "Description", field: "description" },
+    { headerName: "Course Code", field: "courseCode" },
+    { headerName: "Category", field: "category.name" },
+  ];
+
+  return (
+    <div className="quizzes-container">
+      <Typography variant="h5">Quizzes</Typography>
+      <div
+        className="ag-theme-alpine"
+        style={{ height: 300, width: "100%", marginTop: 10 }}
+      >
+        <AgGridReact
+          rowData={quizzes}
+          columnDefs={colDefs}
+          defaultColDef={{ flex: 1, resizable: true }}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Quizzes;
