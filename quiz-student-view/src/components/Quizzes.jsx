02276edd9365b6062c54ddc4eee2fdda949a3d1a
Annie Tran <tranthilananh105@gmail.com>
2025-05-02 19:50:25 +0300

    add dependecy dayjs to format the createdDate

---
+++ quiz-student-view/src/components/Quizzes.jsx
@@ -0,0 +1,10 @@
+function Quizzes() {
+    return (
+        <div>
+            <h1>Quizzes</h1>
+            <p>List of quizzes will be displayed here.</p>
+        </div>
+    );
+}
+
+export default Quizzes;
