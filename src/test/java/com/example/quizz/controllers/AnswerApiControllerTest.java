c59b71ce3bb1605a54cb065054488b2ca9cd3cd8
Dozy <dotuan14747@gmail.com>
2025-05-10 08:16:13 +0300

    Added tests for Answer API

--- src/test/java/com/example/quizz/controllers/AnswerApiControllerTest.java
+++ src/test/java/com/example/quizz/controllers/AnswerApiControllerTest.java
@@ -14,12 +14,13 @@ import com.example.quizz.domain.Question;
 import com.example.quizz.domain.QuestionRepository;
 import com.example.quizz.domain.Quiz;
 import com.example.quizz.domain.QuizRepository;
+import com.example.quizz.domain.StudentAnswer;
+import com.example.quizz.domain.StudentAnswerRepository;
 import com.example.quizz.dto.CreateAnswerOptionDTO;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 import java.util.List;
-import static org.hamcrest.Matchers.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
@@ -34,6 +35,9 @@ public class AnswerApiControllerTest {
 
     @Autowired
     QuestionRepository questionRepository;
+
+    @Autowired
+    StudentAnswerRepository studentAnswerRepository;
 
     @Autowired
     private MockMvc mockMvc;
@@ -73,13 +77,112 @@ public class AnswerApiControllerTest {
 
 
         mockMvc.perform(post("/api/questions/"+ savedQuestion.getId() + "/answers")
-        .contentType(MediaType.APPLICATION_JSON)
-        .content(requestBody))
-        .andExpect(status().is2xxSuccessful());
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(requestBody))
+                .andExpect(status().is2xxSuccessful());
 
         List<Answer> savedAnswers = answerRepository.findAll();
         assertEquals(1, savedAnswers.size());
         assertEquals("Option 1", savedAnswers.get(0).getAnswerText());
+    }
+
+    @Test
+    public void createAnswerDoesNotSaveAnswerWithoutAnswerOption() throws Exception {
+        Quiz quiz = new Quiz();
+        quiz.setName("Test Quiz");
+        quiz.setDescription("Test Description");
+        quiz.setPublished(true);
+        Quiz savedQuiz = quizRepository.save(quiz);
+
+        Question question = new Question();
+        question.setQuiz(savedQuiz);
+        question.setQuestionText("1 + 1 = ?");
+        question.setDifficulty(Question.Difficulty.EASY);
+        Question savedQuestion = questionRepository.save(question);
+
+        Answer answer = new Answer();
+        answer.setQuestion(savedQuestion);
+        answer.setAnswerText("Option 1");
+        answer.setIsCorrect(true);
+        answerRepository.save(answer);
+
+        CreateAnswerOptionDTO answerOptionDTO = new CreateAnswerOptionDTO();
+        answerOptionDTO.setAnswerOptionId(null);
+        String requestBody = mapper.writeValueAsString(answerOptionDTO);
+
+        mockMvc.perform(post("/api/questions/"+ savedQuestion.getId() + "/answers")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(requestBody))
+                .andExpect(status().isBadRequest());
+
+        List<StudentAnswer> studentAnswer = studentAnswerRepository.findAll();
+        assertEquals(0, studentAnswer.size());
+    }
+
+    @Test
+    public void createAnswerDoesNotSaveAnswerForNonExistingAnswerOption() throws Exception {
+        Quiz quiz = new Quiz();
+        quiz.setName("Test Quiz");
+        quiz.setDescription("Test Description");
+        quiz.setPublished(true);
+        Quiz savedQuiz = quizRepository.save(quiz);
+
+        Question question = new Question();
+        question.setQuiz(savedQuiz);
+        question.setQuestionText("1 + 1 = ?");
+        question.setDifficulty(Question.Difficulty.EASY);
+        Question savedQuestion = questionRepository.save(question);
+
+        Answer answer = new Answer();
+        answer.setQuestion(savedQuestion);
+        answer.setAnswerText("Option 1");
+        answer.setIsCorrect(true);
+        answerRepository.save(answer);
+
+        CreateAnswerOptionDTO answerOptionDTO = new CreateAnswerOptionDTO();
+        answerOptionDTO.setAnswerOptionId(999L); // Non-existing ID
+        String requestBody = mapper.writeValueAsString(answerOptionDTO);
+
+        mockMvc.perform(post("/api/questions/"+ savedQuestion.getId() + "/answers")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(requestBody))
+                .andExpect(status().is4xxClientError());
+
+        List<StudentAnswer> studentAnswer = studentAnswerRepository.findAll();
+        assertEquals(0, studentAnswer.size());
+    }
+
+    @Test
+    public void createAnswerDoesNotSaveAnswerForNonPublishedQuiz() throws Exception {
+        Quiz quiz = new Quiz();
+        quiz.setName("Test Quiz");
+        quiz.setDescription("Test Description");
+        quiz.setPublished(false);
+        Quiz savedQuiz = quizRepository.save(quiz);
+
+        Question question = new Question();
+        question.setQuiz(savedQuiz);
+        question.setQuestionText("1 + 1 = ?");
+        question.setDifficulty(Question.Difficulty.EASY);
+        Question savedQuestion = questionRepository.save(question);
+
+        Answer answer = new Answer();
+        answer.setQuestion(savedQuestion);
+        answer.setAnswerText("Option 1");
+        answer.setIsCorrect(true);
+        answerRepository.save(answer);
+
+        CreateAnswerOptionDTO answerOptionDTO = new CreateAnswerOptionDTO();
+        answerOptionDTO.setAnswerOptionId(answer.getId());
+        String requestBody = mapper.writeValueAsString(answerOptionDTO);
+
+        mockMvc.perform(post("/api/questions/"+ savedQuestion.getId() + "/answers")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(requestBody))
+                .andExpect(status().is4xxClientError());
+
+        List<StudentAnswer> studentAnswer = studentAnswerRepository.findAll();
+        assertEquals(0, studentAnswer.size());
     }
 }
 
